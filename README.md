b3796d7a5d324d32815e22a69bfdd2d0ca693423
Johannes Geisslinger <johannes.geisslinger@gwce.de>
2022-01-17 18:13:44 +0100

    final

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-#  :desktop_computer: Winstart 
-
+# :desktop_computer: Winstart 
 This is a simple bash file to automatically connect to an OpenVPN server and mount shared drives. This little script could be used in company or private environment to simplify the startup.
 
-## Configuration
+## :sparkles: How it works
+You can define as many mount points as you like by using the array provided under NAS Drives. The script will check in several ping routines if the internal host is available and if yes, it will mount the drives or if not, connect to VPN first. In this way you can use the script at home / office AND remote. Have fun!
 
-You need to
+
+## :wrench: Configuration
+
+You need to configure the following things to make the script work
+
+[x] Install OpenVPN GUI (Community Edition) and install profile
+[x] Setup SMB / share user with access to requested drives/folders (password without special characters)
+[x] Set Variables in the script to match your confirguration
+
+
+## :gem: Credits
+The script uses the ANSI snippet from @mlocati to make the terminal more colorful :rainbow:. Thanks for that.
--- connect.bat
+++ connect.bat
@@ -5,10 +5,10 @@ setlocal enabledelayedexpansion
 call :setESC
 cls
 :: Set Variables
-set "INTERNHOST=196.192.10.1"
-set "VPNPROFIL=Profil.ovpn"
-set "USER=Praxis"
-set "PW=sonnen!/schein"
+set NTERNHOST=196.192.10.1
+set VPNPROFIL=Profil.ovpn
+set USER=YOURUSERHERE
+set PW=YOURPASSWORD
 
 :: NAS Drives
 set DRIVE[0]=B:
@@ -16,27 +16,27 @@ set DRIVE[1]=Z:
 set DRIVE[2]=Y:
 set DRIVE[3]=X:
 
-set DRIVEPATH[0]="\\%INTERNHOST%\Server_Backup"
-set DRIVEPATH[1]="\\%INTERNHOST%\Server_Praxis"
-set DRIVEPATH[2]="\\%INTERNHOST%\Server_B.Farris"
-set DRIVEPATH[3]="\\%INTERNHOST%\Server_S.Farris"
+set DRIVEPATH[0]="\\%INTERNHOST%\FolderA"
+set DRIVEPATH[1]="\\%INTERNHOST%\FolderB"
+set DRIVEPATH[2]="\\%INTERNHOST%\FolderC"
+set DRIVEPATH[3]="\\%INTERNHOST%\FolderD"
 
 :: Start Routine
-echo %ESC%[7m      VERBINDUNGSMANAGER v1.0             %ESC%[0m
+echo %ESC%[7m      CONNECTMANAGER v1.0             %ESC%[0m
 rem
-echo ::: Pruefen ob im internen Netz
+echo ::: Check if internal network
 ping 127.0.0.1 -n 2 > nul
-echo %ESC%[42mOK%ESC%[0m    -     Anfrage an Server senden
+echo %ESC%[42mOK%ESC%[0m    -     Send request to server
 ping -n 1 "%INTERNHOST%" | findstr /r /c:"[0-9] *ms" > nul
 
 if !ERRORLEVEL!  == 0 (
     
     ping 127.0.0.1 -n 2 > nul
     :: If Computer in interal network   
-    echo %ESC%[42mOK%ESC%[0m    -     Verbindung erfolgreich. Keine VPN Verbindung notwendig.
+    echo %ESC%[42mOK%ESC%[0m    -     Connection confirmed. No VPN necessary.
     
     rem
-    echo ::: Verbinden von Laufwerken
+    echo ::: Mount Drives
     ping 127.0.0.1 -n 2 > nul
 
     :: CONNECT DRIVES
@@ -44,28 +44,28 @@ if !ERRORLEVEL!  == 0 (
 
        net use !DRIVE[%%n]! /delete /Y > NUL 2>&1 || dir > NUL
        net use !DRIVE[%%n]! !DRIVEPATH[%%n]! /user:%USER% %PW% /PERSISTENT:NO > nul
-       echo %ESC%[42mOK%ESC%[0m    -     Laufwerk !DRIVE[%%n]! verbunden 
+       echo %ESC%[42mOK%ESC%[0m    -     Drive !DRIVE[%%n]! connected 
 
     )
        
     rem
-    echo %ESC%[7mAlles erfolgreich. Dieses Fenster schliesst sich in 5 Sekunden.%ESC%[0m
+    echo %ESC%[7m Everything ok. Window will be closed in 5 seconds %ESC%[0m
     ping 127.0.0.1 -n 5 > nul
 
 ) else (
 
     :: If Computer remote
-    echo %ESC%[43mNO%ESC%[0m    -     Nicht im Netzwerk. VPN Verbindung ist erforderlich.
+    echo %ESC%[43mNO%ESC%[0m    -     Not in network. VPN connection necessary
     ping 127.0.0.1 -n 1 > nul
     rem
-    echo ::: Pruefen der Internetverbindung
-    echo %ESC%[42mOK%ESC%[0m    -     Testen der Internetverbindung
+    echo ::: Test of internet connection
+    echo %ESC%[42mOK%ESC%[0m    -     Test of internet connection initiated
     ping -n 1 "1.1.1.1" | findstr /r /c:"[0-9] *ms" > nul
     
     if !ERRORLEVEL! == 0 (
     :: If Internet connection is ok
           ping 127.0.0.1 -n 2 > nul
-          echo %ESC%[42mOK%ESC%[0m    -     Verbindung ist in Ordnung
+          echo %ESC%[42mOK%ESC%[0m    -     Connection test sucessfull
           ping 127.0.0.1 -n 2 > nul
           
           rem VPN Verbindung herstellen
@@ -78,10 +78,10 @@ if !ERRORLEVEL!  == 0 (
 
           if !ERRORLEVEL! == 0 (
 
-             echo %ESC%[42mOK%ESC%[0m    -     VPN Verbindung hergestellt
+             echo %ESC%[42mOK%ESC%[0m    -     VPN connection established
              
              rem
-             echo ::: Verbinden von Laufwerken  
+             echo ::: Mount Drives
              ping 127.0.0.1 -n 2 > nul
 
              :: CONNECT DRIVES
@@ -89,24 +89,24 @@ if !ERRORLEVEL!  == 0 (
 
                net use !DRIVE[%%n]! /delete /Y > NUL 2>&1 || dir > NUL
                net use !DRIVE[%%n]! !DRIVEPATH[%%n]! /user:%USER% %PW% /PERSISTENT:NO > nul
-               echo %ESC%[42mOK%ESC%[0m    -     Laufwerk !DRIVE[%%n]! verbunden 
+               echo %ESC%[42mOK%ESC%[0m    -     Drive !DRIVE[%%n]! connected 
 
             )
 
              rem
-             echo %ESC%[7mAlles erfolgreich. Dieses Fenster schliesst sich in 5 Sekunden.%ESC%[0m
+             echo %ESC%[7m Everything ok. Window will be closed in 5 seconds %ESC%[0m
              ping 127.0.0.1 -n 5 > nul
     
           ) else (
 
-             echo %ESC%[41mER%ESC%[0m    -     Problem mit VPN Verbindung. Bitte erneut probieren.
+             echo %ESC%[41mER%ESC%[0m    -     Error with VPN connection. Please try again.
              rem
              @pause
           )
       
     ) else (
     :: If Internet Connection failed 
-      echo %ESC%[41mER%ESC%[0m    -     Verbindung fehlgeschlagen. Internetverbindung fehlerhaft.
+      echo %ESC%[41mER%ESC%[0m    -     Error in connection test. Please check internet availability.
       rem
       @pause
     )
